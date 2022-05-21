d03f940d10aa2949c5d3558a41c8ee564cccd262
Redwoub <eloiverinaudcoma@gmail.com>
2022-05-21 15:21:07 +0100

    [add] config.yml

--- src/fr/redwoub/utils/Main.java
+++ src/fr/redwoub/utils/Main.java
@@ -1,30 +1,31 @@
 package fr.redwoub.utils;
 
-import fr.redwoub.utils.commands.*;
-import fr.redwoub.utils.listeners.PlayerInterract;
-import fr.redwoub.utils.listeners.PlayerQuit;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import fr.redwoub.utils.commands.*;
+import fr.redwoub.utils.listeners.PlayerJoin;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.*;
 
 public class Main extends JavaPlugin {
 
-    public static List<UUID> flying = new ArrayList<>();
+    public WeakHashMap<UUID, Boolean> flying = new WeakHashMap<>();
+    public static Main instance;
 
     @Override
     public void onEnable() {
+        instance = this;
+
         getCommand("bc").setExecutor(new BcCMD());
         getCommand("gm").setExecutor(new GmCMD());
         getCommand("skull").setExecutor(new SkullCMD());
         getCommand("discord").setExecutor(new DiscordCMD());
         getCommand("fly").setExecutor(new FlyCMD());
 
-        getServer().getPluginManager().registerEvents(new PlayerQuit(), this);
-        getServer().getPluginManager().registerEvents(new PlayerInterract(), this);
+        getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
+
     }
-        ConfigurationSection
+
+    public static Main getInstance() {
+        return instance;
+    }
 }
