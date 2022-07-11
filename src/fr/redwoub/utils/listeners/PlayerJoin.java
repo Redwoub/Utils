a0ad7d22fecbafe76ab24f437d4a66d9daac833a
Redwoub <eloiverinaudcoma@gmail.com>
2022-07-11 14:44:54 +0100

    [remove] useless imports

--- src/fr/redwoub/utils/listeners/PlayerJoin.java
+++ src/fr/redwoub/utils/listeners/PlayerJoin.java
@@ -1,11 +1,9 @@
 package fr.redwoub.utils.listeners;
 
 import fr.redwoub.utils.Main;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerJoin implements Listener {
 
