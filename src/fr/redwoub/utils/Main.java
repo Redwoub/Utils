ac204f8149dc2eea59786b07782b180eafa17138
Redwoub <eloiverinaudcoma@gmail.com>
2022-07-22 22:20:37 +0100

    [add] file for save msg option and register togglemsg command

--- src/fr/redwoub/utils/Main.java
+++ src/fr/redwoub/utils/Main.java
@@ -4,17 +4,32 @@ package fr.redwoub.utils;
 import fr.redwoub.utils.commands.*;
 import fr.redwoub.utils.listeners.PlayerJoin;
 import fr.redwoub.utils.listeners.PlayerQuit;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 public class Main extends JavaPlugin {
 
     public WeakHashMap<UUID, Boolean> flying = new WeakHashMap<>();
+    public File file = new File(getDataFolder(), "msg.yml");
     public static Main instance;
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
         instance = this;
+        if(!getDataFolder().exists()) getDataFolder().mkdir();
+
+        if(!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
 
         getCommand("bc").setExecutor(new BcCMD());
         getCommand("gm").setExecutor(new GmCMD());
@@ -22,14 +37,18 @@ public class Main extends JavaPlugin {
         getCommand("discord").setExecutor(new DiscordCMD());
         getCommand("fly").setExecutor(new FlyCMD());
         getCommand("msg").setExecutor(new MsgCMD());
+        getCommand("togglemsg").setExecutor(new ToggleMsgCMD());
 
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
         getServer().getPluginManager().registerEvents(new PlayerQuit(), this);
 
-        saveDefaultConfig();
     }
 
     public static Main getInstance() {
         return instance;
+    }
+
+    public File getFile() {
+        return file;
     }
 }
