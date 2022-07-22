f6ac2c0722a7af3d6dd0f1461a21be9f3629fc34
Redwoub <eloiverinaudcoma@gmail.com>
2022-07-22 22:16:19 +0100

    [update] error message now configurable

--- src/fr/redwoub/utils/commands/BcCMD.java
+++ src/fr/redwoub/utils/commands/BcCMD.java
@@ -6,21 +6,24 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 public class BcCMD implements CommandExecutor {
+    private final String prefix = ChatColor.translateAlternateColorCodes('&', Main.getInstance().getConfig().getString("messages.prefix"));
+    private final FileConfiguration config = Main.getInstance().getConfig();
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
         StringBuilder stringBuilder;
 
         if(!sender.hasPermission(Main.getInstance().getConfig().getString("permissions.bc"))){
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.getInstance().getConfig().getString("messages.dont-have-permission")));
+            sender.sendMessage(prefix + " " + ChatColor.translateAlternateColorCodes('&', Main.getInstance().getConfig().getString("messages.dont-have-permission")));
             return false;
         }
 
         if(args.length == 0){
-            sender.sendMessage(ChatColor.RED + "The commande is : /bc <message>");
+            sender.sendMessage(prefix + " " + ChatColor.translateAlternateColorCodes('&', config.getString("errors.bc-invalid-arguments")));
             return false;
         }
         stringBuilder = new StringBuilder();
