f4027a40426306ef144c68b7a8e70c881a2ded7b
Redwoub <eloiverinaudcoma@gmail.com>
2022-07-22 22:17:39 +0100

    [add] possibility to configure format of msg

--- src/fr/redwoub/utils/commands/MsgCMD.java
+++ src/fr/redwoub/utils/commands/MsgCMD.java
@@ -7,6 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 public class MsgCMD implements CommandExecutor {
@@ -22,8 +23,8 @@ public class MsgCMD implements CommandExecutor {
         }
 
         Player player = (Player) sender;
-        if(args.length < 3){
-            player.sendMessage(prefix + " §cError §8: §e/msg <Player> <Message>");
+        if(args.length < 2){
+            player.sendMessage(prefix + " " + ChatColor.translateAlternateColorCodes('&', config.getString("errors.msg-invalid-arguments")));
             return false;
         }
 
@@ -38,13 +39,24 @@ public class MsgCMD implements CommandExecutor {
         }
 
         Player target = Bukkit.getPlayer(args[0]);
+        YamlConfiguration data = YamlConfiguration.loadConfiguration(Main.instance.getFile());
+        if(data.getString(target.getName()).equalsIgnoreCase("false")){
+            player.sendMessage(prefix + " " + ChatColor.translateAlternateColorCodes('&', config.getString("messages.player-disable-msg")));
+            return false;
+        }
+
         StringBuilder stringBuilder = new StringBuilder();
         for(int i = 1; i < args.length; i++){
             stringBuilder.append(args[i]).append(" ");
         }
 
-        target.sendMessage(prefix + " §bFrom " + player.getDisplayName() + " §8: §7" + stringBuilder.toString());
-        player.sendMessage(prefix + " §bTo " + target.getDisplayName() + " §8: §7" + stringBuilder.toString());
+        if(Main.getInstance().getConfig().getBoolean("msg.use-display-name")){
+            target.sendMessage(prefix + " " + ChatColor.translateAlternateColorCodes('&', config.getString("msg.target-recive-message-format").replace("%p", player.getDisplayName()).replace("%m", stringBuilder.toString())));
+            player.sendMessage(prefix + " " + ChatColor.translateAlternateColorCodes('&', config.getString("msg.player-recive-message-format").replace("%p", target.getDisplayName()).replace("%m", stringBuilder.toString())));
+        } else {
+            target.sendMessage(prefix + " " + ChatColor.translateAlternateColorCodes('&', config.getString("msg.target-recive-message-format").replace("%p", player.getName()).replace("%m", stringBuilder.toString())));
+            player.sendMessage(prefix + " " + ChatColor.translateAlternateColorCodes('&', config.getString("msg.player-recive-message-format").replace("%p", target.getName()).replace("%m", stringBuilder.toString())));
+        }
         return false;
     }
 }
